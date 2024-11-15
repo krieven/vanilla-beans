9f6199e08022070c57769599615a24afea0129e6
vitaly <vitalydmitriev1970@gmail.com>
2024-11-16 01:50:59 +0300

    builder

--- test/testBuilder.mjs
+++ test/testBuilder.mjs
@@ -2,6 +2,6 @@ import builder from '../builder/builder.mjs'
 import path from 'path'
 
 builder(
-    path.resolve('docs/examples/logo/beans/logo.beans.htm'), 
+    path.resolve('docs/examples/logo/beans/logo.beans.htm'),
     path.resolve('test/generated/logo.beans.js')
 )
